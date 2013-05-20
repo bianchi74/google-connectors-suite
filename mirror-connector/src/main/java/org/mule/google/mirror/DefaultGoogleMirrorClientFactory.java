a9018119d4561ad74727c62ac75f0021559055e1
Ross Mason <ross@mulesoft.com>
2013-05-20 08:55:09 -0700

    First drop of the mirror connector. The test case is currently failing, so please lower your expectation!

---
+++ mirror-connector/src/main/java/org/mule/google/mirror/DefaultGoogleMirrorClientFactory.java
@@ -0,0 +1,34 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.google.mirror;
+
+import org.mule.modules.google.oauth.invalidation.InvalidationAwareCredential;
+
+import com.google.api.client.auth.oauth2.BearerToken;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson.JacksonFactory;
+import com.google.api.services.mirror.Mirror;
+
+/**
+ * TODO
+ */
+public class DefaultGoogleMirrorClientFactory implements GoogleMirrorClientFactory
+{
+    public Mirror newClient(String accessToken, String applicationName)
+    {
+        Credential credential = new InvalidationAwareCredential(BearerToken.authorizationHeaderAccessMethod());
+        credential.setAccessToken(accessToken);
+
+        return new Mirror.Builder(new NetHttpTransport(), new JacksonFactory(), credential)
+                .setApplicationName(applicationName)
+                .build();
+    }
+}
